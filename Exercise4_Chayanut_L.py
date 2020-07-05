abc588471b8b7dfda87cea0969adaf7749b77ee6
icelnwza <61050160@kmitl.ac.th>
2020-07-05 23:26:10 +0700

    Create Exercise4_Chayanut_L.py

---
+++ Exercise4_Chayanut_L.py
@@ -0,0 +1,9 @@
+FoundEng = 30
+GenBus = 25
+ComSys = 28.75
+ComPro = 21.34
+print("--- Your Score ---")
+print("Foundaton English : ", FoundEng)
+print("General Business : ", GenBus)
+print("Introduction to Computer Systems : ", ComSys)
+print("Computer Programming : ", ComPro)
