65f6f48ac738276fde7eb8a5624d4f456e5f9c88
icelnwza <61050160@kmitl.ac.th>
2020-07-12 19:58:45 +0700

    Create Lecture73_Chayanut_L.py

---
+++ Lecture73_Chayanut_L.py
@@ -0,0 +1,23 @@
+systemMenu = {"ไก่ทอด": 35, "เป็ดทอด": 45, "ปลาทอด": 55, "ผักทอด": 20}
+menuList = []
+
+def showBill():
+    print("---- My Food----")
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+    totalPrice()
+
+def totalPrice():
+    result = 0
+    for i in range(len(menuList)):
+         result = result + menuList[i][1]
+    print("Total Price =",result)
+
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+
+showBill()
