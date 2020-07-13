5594f53630dc469fe321d93836e09c269ce993a3
icelnwza <61050160@kmitl.ac.th>
2020-07-13 23:57:58 +0700

    Update lecture90_Chayanut_L.py

--- lecture90_Chayanut_L.py
+++ lecture90_Chayanut_L.py
@@ -4,21 +4,23 @@ import math
 
 def calBMI(event):
     resultCal = float(BoxWeight.get())/(math.pow(float(BoxHeight.get())/100,2))
+
+
     if resultCal >= 30:
-        print("อ้วนมากกกก",resultCal)
-        labelResult.configure(text="อ้วนมากกกก",font = ("Angsana,10"),fg = "red")
+        print("อ้วนมากกกก %.2f" % (resultCal))
+        labelResult.configure(text=("อ้วนมากกกก BMI = %.2f " % (resultCal)),font = ("Angsana,10"),fg = "red")
     elif resultCal < 30 and resultCal >=25:
         print("อ้วน", resultCal)
-        labelResult.configure(text="อ้วน", font=("Angsana,10"),fg = "red")
+        labelResult.configure(text=("อ้วน BMI = %.2f " % (resultCal)), font=("Angsana,10"),fg = "red")
     elif resultCal < 25 and resultCal >= 23:
         print("น้ำหนักเกิน", resultCal)
-        labelResult.configure(text="น้ำหนักเกิน", font=("Angsana,10"),fg = "red")
+        labelResult.configure(text=("น้ำหนักเกิน BMI = %.2f "% (resultCal)), font=("Angsana,10"),fg = "red")
     elif resultCal < 23 and resultCal >= 18.6:
         print("น้ำหนักเหมาะสม", resultCal)
-        labelResult.configure(text="น้ำหนักเหมาะสม", font=("Angsana,10"),fg = "red")
+        labelResult.configure(text=("น้ำหนักเหมาะสม BMI = %.2f "% (resultCal)), font=("Angsana,10"),fg = "red")
     else:
         print("ผอมเกิน", resultCal)
-        labelResult.configure(text="ผอมเกิน", font=("Angsana,10"),fg = "red")
+        labelResult.configure(text=("ผอมเกิน BMI = %.2f "% (resultCal)), font=("Angsana,10"),fg = "red")
 
 
 mainWindow = Tk()
